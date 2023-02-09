7f6d5cb7d438b81fb4a825346d9fc011a75a923c
William Oliveira <w.oliveira542@gmail.com>
2023-02-09 11:28:44 -0300

    feat: add create-next-app

--- next-sample/pages/github.jsx
+++ next-sample/pages/github.jsx
@@ -1,29 +1,31 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 // Example: http://localhost:3000/github?user=woliveiras
 
 const GitHub = ({ userData }) => {
-    const router = useRouter()
-    const { user } = router.query
-  
-    return (
-        <>
-            <strong>Query: {user}</strong>
-            <p>User from GitHub: {userData.name}</p>
-        </>
-    )
-  }
-  
-export default GitHub
+  const router = useRouter();
+  const { user } = router.query;
+
+  return (
+    <>
+      <strong>Query: {user}</strong>
+      <p>User from GitHub: {userData.name}</p>
+    </>
+  );
+};
+
+export default GitHub;
 
 export async function getServerSideProps({ query }) {
-    const { user } = query
-    
-    const userData = await (await fetch(`https://api.github.com/users/${user}`)).json();
+  const { user } = query;
 
-    return {
-        props: {
-            userData
-        }, 
-    }
+  const userData = await (
+    await fetch(`https://api.github.com/users/${user}`)
+  ).json();
+
+  return {
+    props: {
+      userData,
+    },
+  };
 }
